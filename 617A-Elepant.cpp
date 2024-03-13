5721c4784b98d8efbe204a499ea923c5bd481db2
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-03-13 22:31:30 +0600

    617A-Elepant.cpp

---
+++ 617A-Elepant.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+        int a;
+        cin>>a;
+        if(a%5==0)
+        {
+            cout<<a/5;
+        }
+        else
+        {
+            cout<<a/5+1<<endl;
+        }
+        return 0;
+
+}
