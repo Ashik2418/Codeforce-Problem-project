34704d2c2331ad20a6c845e1ba63d06d2b56265d
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-03-19 00:35:19 +0600

    Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    cout<<"\n";
+    if(w<=2||w%2!=0)
+    {
+        cout<<"No"<<endl;
+    }
+    else{
+        cout<<"Yes"<<endl;
+    }
+ 
+}
