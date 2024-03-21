4ccaad327490b04e207e369a878223c20ef6e789
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-03-21 23:50:56 +0600

    1030A-In Search of an Easy Problem.cpp

---
+++ 1030A-In Search of an Easy Problem.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+        int n,count1;
+        cin>>n;
+        for(int j=0;j<n;j++)
+        {
+            cin>>count1;
+            if(count1==1)
+            {
+                cout<<"HARD"<<endl;
+                return 0;
+            }
+
+        }
+        cout<<"EASY";
+        return 0;
+
+
+
+}
