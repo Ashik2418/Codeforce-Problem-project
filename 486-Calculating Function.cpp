ab4ea32565fea450649959677c74a0e384f7864d
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-04-03 00:30:44 +0600

    486-Calculating Function.cpp

---
+++ 486-Calculating Function.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+         int n,m;
+         cin>>n;
+
+
+             if(n%2==0)
+             {
+                 cout<<n/2;
+
+
+             }
+             else
+             {
+                 m=-(n/2+1);
+                 cout<<m;
+             }
+
+
+
+         return 0;
+
+}
