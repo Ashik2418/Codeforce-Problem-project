35f05f388f5c5f1c3c422be01b23161c6bbedc33
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-05-10 01:30:07 +0600

    Create A. ICPC মানেই BUBT?.cpp

---
+++ BIUPC/A. ICPC মানেই BUBT?.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+           int n;
+           n=3;
+           cout<<n;
+}
