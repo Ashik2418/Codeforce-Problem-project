285b91f7a0aa57edd355fa185b015eb3b7cb99ef
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-09-03 23:42:01 +0600

    A - Minimize!.cpp

---
+++ Div-4 Contest/A - Minimize!.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+           int a,b,c,n;
+           cin>>n;
+
+           while(n--)
+           {
+               cin>>a>>b;
+               int minvalue= b-a;
+               cout<<minvalue<<endl;
+           }
+
+
+           return 0;
+}
