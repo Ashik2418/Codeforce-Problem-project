fb4e8fba41cd378632e4999738614e3353f4ffcd
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-11-20 22:43:43 +0600

    A - Behind The Story.cpp

---
+++ BIUPC/BIUPC 2024/A - Behind The Story.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+
+     int n=1;
+     cout<<n;
+     return 0;
+
+
+}
