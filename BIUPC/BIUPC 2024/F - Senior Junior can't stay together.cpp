f4fc09705b758a7caf24cac3789d47db79f6cdbe
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-11-20 22:47:54 +0600

    F - Senior Junior can't stay together.cpp

---
+++ BIUPC/BIUPC 2024/F - Senior Junior can't stay together.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+      int n;
+      cin>>n;
+      if(n==48 || n== 49 || n==50)
+      {
+          cout<<"Probin";
+      }
+      else if(n==51 || n==52 || n==53)
+      {
+          cout<<"Nobin";
+      }
+      else
+      {
+          cout<<"Bohiragoto";
+      }
+    return 0;
+}
