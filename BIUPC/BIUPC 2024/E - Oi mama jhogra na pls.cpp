304ddbfce992bf99475ac00f9b368b2ffa3a477b
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-11-21 23:20:45 +0600

    E - Oi mama jhogra na pls.cpp

---
+++ BIUPC/BIUPC 2024/E - Oi mama jhogra na pls.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+         int n;
+         cin>>n;
+         if(n%2 == 0)
+         {
+             cout<<"YES";
+         }
+         else
+         {
+             cout<<"NO";
+         }
+         return 0;
+}
