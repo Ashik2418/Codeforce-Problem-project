32900f64e0f54608de10c4c3267aeab1d82d6f95
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2025-04-20 00:00:03 +0600

    A. Trippi Troppi.cpp

---
+++ Div-4 Contest/A. Trippi Troppi.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+signed main(){
+opti mise
+
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         string a,b,c;
+         cin>>a>>b>>c;
+         cout<<a[0]<<b[0]<<c[0]<<endl;
+     }
+     return 0;
+}
